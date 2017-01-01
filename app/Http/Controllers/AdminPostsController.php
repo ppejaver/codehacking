cf193641394c3f3e81c693269cd1227e95131f1f
ppejaver <ppejaver@gmail.com>
2017-01-01 10:27:36 +0530

    able to create postwith status category

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\PostsCreateRequest;
+use App\Post;
+use App\Photo;
+use Illuminate\Support\Facades\Auth;
 
 class AdminPostsController extends Controller
 {
@@ -16,7 +20,8 @@ class AdminPostsController extends Controller
     public function index()
     {
         //
-		return view('admin.posts.index');
+		$posts = Post::all();
+		return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -36,9 +41,25 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
         //
+		$input = $request->all();
+		$user = Auth::user();
+		
+		if($file = $request->file('photo_id')) {
+			$name = time().$file->getClientOriginalName();
+			$file->move('images', $name);
+			$photo = Photo::create(['file'=>$name]);
+			$input['photo_id'] = $photo->id;
+		}
+		$user->posts()->create($input);
+		
+		return redirect('/admin/posts');
+		
+		
+		
+		
     }
 
     /**
